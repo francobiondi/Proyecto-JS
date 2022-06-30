1bd676fe4c46781529021c0177084bbdd6cf0304
Franco Biondi <francobiondi45@gmail.com>
2022-06-29 22:28:39 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let remeras = 1;
+let disponibles = "Remeras en stock"
+
+
+while (remeras <=20) {
+    console.log(`La cantidad de remeras disponibles es de: ${remeras}`)
+    remeras++;
+}
+
+let resultado = remeras + " " + disponibles
+alert(resultado)
+
